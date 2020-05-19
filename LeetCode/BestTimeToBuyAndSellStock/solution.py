a881ea84f2a529f659fcd6e4930e8187eaeaa3d7
YooyeonHwang <50202274+uyeonH@users.noreply.github.com>
2020-05-19 19:47:56 +0900

    Update solution.py

--- LeetCode/BestTimeToBuyAndSellStock/solution.py
+++ LeetCode/BestTimeToBuyAndSellStock/solution.py
@@ -1,1 +1,21 @@
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices)==0:
+            return 0
+        
+        buy = prices[0]
+        mx_profit = 0
+
+        for i in range(1,len(prices)):
+            
+            profit = prices[i]-buy
+            
+            if profit>mx_profit:
+                mx_profit = profit
+
+            if buy>prices[i]:
+                buy = prices[i]
+        
+        return mx_profit
+    
