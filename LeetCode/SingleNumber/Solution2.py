9861f63c317942f20e6306ab50bb2b3666df56f4
YooyeonHwang <50202274+uyeonH@users.noreply.github.com>
2020-05-23 20:23:20 +0900

    Update Solution2.py

--- LeetCode/SingleNumber/Solution2.py
+++ LeetCode/SingleNumber/Solution2.py
@@ -7,5 +7,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        # (중복을 제거한 배열 요소의 합 * 2) - (배열 요소의 합)
         return 2 * sum(set(nums)) - sum(nums)
