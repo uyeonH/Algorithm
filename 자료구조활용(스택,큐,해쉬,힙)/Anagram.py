c7e07ccfd6b3fd3512eed5dfb6e96d030c3e9bb2
YooyeonHwang <50202274+uyeonH@users.noreply.github.com>
2020-12-12 20:24:02 +0900

    Update Anagram.py

--- 자료구조활용(스택,큐,해쉬,힙)/Anagram.py
+++ 자료구조활용(스택,큐,해쉬,힙)/Anagram.py
@@ -6,7 +6,52 @@ XbCdfGhiNoPuwUyABcDEgHKkOtQRxW
 
 import sys
 
-sys.stdin = open("in4.txt", "r")
+sys.stdin = open("in1.txt", "r")
+
+# 풀이3
+a = input()
+b = input()
+str1 = [0] * 52
+str2 = [0] * 52
+# 소문자 -71
+# 대문자 -65
+for x in a:
+    if x.isupper():
+        str1[ord(x) - 65] += 1  # 아스키넘버
+    elif x.islower():
+        str1[ord(x) - 71] += 1
+for x in b:
+    if x.isupper():
+        str2[ord(x) - 65] += 1  # 아스키넘버
+    elif x.islower():
+        str2[ord(x) - 71] += 1
+for i in range(52):
+    if str1[i]!=str2[i]:
+        print("NO")
+        break
+else:
+    print("YES")
+
+'''
+# 풀이2
+a=input()
+b=input()
+sH=dict()
+for x in a:
+    sH[x]=sH.get(x,0)+1
+for x in b:
+    sH[x]=sH.get(x,0)-1
+print(sH)
+for x in a:
+    if sH.get(x)!=0:
+        print("NO")
+        break
+else:
+    print("YES")
+'''
+
+'''
+# 풀이1
 n = input()
 m = input()
 str1 = dict()
@@ -16,7 +61,7 @@ str2 = dict()
 for x in n:
     str1[x] = str1.get(x,0)+1 # key값이 있으면 그 val값을, 없으면 0을 리턴하는 함수
 for x in m:
-    str2[x] = str2.get(x,0)+1
+    str2[x] = str2.get(x,0)+1 # key값이 있으면 그 val값을, 없으면 0을 리턴하는 함수
 for i in str1.keys():
     if i in str2.keys():
         if str1[i]!=str2[i]:
@@ -27,3 +72,4 @@ for i in str1.keys():
         break
 else:
     print("YES")
+'''
