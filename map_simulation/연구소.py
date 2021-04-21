05c5cd15ee7116f1f097d631de1a7817a755d0ce
YooyeonHwang <50202274+uyeonH@users.noreply.github.com>
2021-04-21 18:34:55 +0900

    Update 연구소.py

--- map_simulation/연구소.py
+++ map_simulation/연구소.py
@@ -1,50 +1,49 @@
 import copy
+import sys
 from collections import deque
 
-
-
-def dfs():
-    global result
+def bfs():
+    global ans
     q = deque(virus)
-    board2 = copy.deepcopy(board)
+    w = copy.deepcopy(board)
     cnt = 0
+
     while q:
         x, y = q.popleft()
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if board2[nx][ny] == 0:
-                board2[nx][ny] = 2
-                q.append([nx, ny])
-    for b in board2:
-        cnt += b.count(0)
-    result = max(result, cnt)
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if w[nx][ny] == 0:
+                    w[nx][ny] = 2
+                    q.append([nx, ny])
+    for i in w:
+        cnt += i.count(0)
+    ans = max(ans, cnt)
 
 
-def wall(p, q, depth):
-    if depth == 3:
-        dfs()
+def wall(p, q, x):
+    if x == 3:  # 벽 3개
+        bfs()
         return
-    else:
-        for i in range(p, n):
-            for j in range(q, m):
-                print(i,j)
-                if board[i][j] == 0:
-                    board[i][j] = 1
-                    wall(i, j, depth + 1)
-                    board[i][j] = 0
-            q = 0
+    for i in range(p, n):
+        for j in range(q, m):
+            if board[i][j] == 0:
+                board[i][j] = 1
+                wall(i, j, x + 1)
+                board[i][j] = 0
+        q = 0
 
-if __name__ == '__main__':
-    n, m = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(n)]
-    # 1: 벽, 2: 바이러스
-    virus = []
-    result = 0
-    dx, dy = (0, 1, 0, -1), (1, 0, -1, 0)
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == 2:
-                virus.append([i, j])
-    wall(0, 0, 0)
-    print(result)
+
+n, m = map(int, sys.stdin.readline().split())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+dx, dy = (1, 0, -1, 0), (0, 1, 0, -1)
+ans = 0
+virus = []
+# 1:벽, 2:바이러스
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 2:
+            virus.append([i, j])
+wall(0, 0, 0)
+print(ans)
